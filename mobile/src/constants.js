11743ff858b5131dd80d48f8edd50beacf1fbab3
banksharp <cbh0102@auburn.edu>
2026-03-03 17:35:35 -0600

    Update subscription product ID to caiddie_pro_monthly

--- mobile/src/constants.js
+++ mobile/src/constants.js
@@ -1,2 +1,2 @@
 // Must match the subscription product ID in App Store Connect (and Google Play if you add Android).
-export const SUBSCRIPTION_PRODUCT_ID = 'ai_caddie_pro_monthly';
+export const SUBSCRIPTION_PRODUCT_ID = 'caiddie_pro_monthly';
